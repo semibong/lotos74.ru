36b942dbc4f6c74a0128718fcce925bd5a760639
KULTIST7 <kultist77@gmail.com>
2025-10-05 16:57:22 +0500

    lk-tests datepicker updated

--- js/lk.js
+++ js/lk.js
@@ -146,9 +146,27 @@ $(document).ready(function () {
     }
 
     if ($('.lk__tests__form').length) {
-        $('.lk__tests__form__date input[type=date]').on('change', function (e) {
-            $(this).next()[0].value = e.target.value;
-        });
+        $.datepicker.regional['ru'] = {
+            closeText: 'Закрыть',
+            prevText: 'Предыдущий',
+            nextText: 'Следующий',
+            currentText: 'Сегодня',
+            monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+            monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+            dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+            dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+            dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+            weekHeader: 'Не',
+            dateFormat: 'dd.mm.yy',
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: ''
+        };
+        $.datepicker.setDefaults($.datepicker.regional['ru']);
+        $('.lk__tests__form__date input').datepicker({
+            maxDate: 0
+	    });
     }
 
     if ($('.lk__test__table').length) {
